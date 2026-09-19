632aebf3610f2fe539160d153abd90f443834409
Saumel-Das <saumel.spr@gmail.com>
2026-09-19 21:12:49 +0600

    solved

--- P_1.js
+++ P_1.js
@@ -1,7 +1,15 @@
-let a = 5;
-let b = 10;
-let temp;
-temp = b;
-b = a;
-a = temp;
-console.log(a, b);
+// Method 1: Using a temporary variable
+// let a = 5;
+// let b = 10;
+// let temp;
+// temp = b;
+// b = a;
+// a = temp;
+// console.log(a, b);
+
+// Method 2: Using array destructuring
+// let a = 5;
+// let b = 10;
+// [a, b] = [b, a];
+// console.log(a, b);
+
