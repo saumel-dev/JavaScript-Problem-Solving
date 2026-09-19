c893eb987b20914298404af1d562412c87a30ecc
Saumel-Das <saumel.spr@gmail.com>
2026-09-19 21:46:50 +0600

    solved

---
+++ P_7.js
@@ -0,0 +1,14 @@
+const isVowel = (ch) => {
+    if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') return true;
+    return false;
+}
+let s = "javascript";
+let cnt = 0;
+for(let i = 0; i < s.length; i++)
+{
+    if(isVowel(s[i]))
+    {
+        cnt++;
+    }
+}
+console.log(cnt);
