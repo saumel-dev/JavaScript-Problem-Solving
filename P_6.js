e7839e602840005456561c5465055a5de0ba5ced
Saumel-Das <saumel.spr@gmail.com>
2026-09-19 21:25:39 +0600

    solved

--- P_6.js
+++ P_6.js
@@ -1,3 +1,7 @@
-let s = "Hello";
-let result_1 = s.split("").reverse().join("");
-console.log(result_1);
+// let s = "Hello";
+// let result_1 = s.split("").reverse().join("");
+// console.log(result_1);
+
+// let s = "hello world";
+// let result_2 = s.split(" ").reverse().join(" ");
+// console.log(result_2);
