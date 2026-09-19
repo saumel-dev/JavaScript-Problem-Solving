e37828ce5ccfc253ce347d18c762623c59f34e0c
Saumel-Das <saumel.spr@gmail.com>
2026-09-19 21:20:56 +0600

    solved

---
+++ P_5.js
@@ -0,0 +1,11 @@
+// function max_of_three(a, b, c) {
+//     return Math.max(a, b, c);
+// }
+
+// let a = 100, b = 200, c = 500; 
+// let result = max_of_three(a, b, c);
+// console.log(result);
+
+// let a = 100, b = 200, c = 500; 
+// let result = Math.max(a, b, c);
+// console.log(result);
