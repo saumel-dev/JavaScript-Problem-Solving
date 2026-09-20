ad5684c485f213e09b28bb92db03f848ff2d9382
Saumel-Das <saumel.spr@gmail.com>
2026-09-20 12:36:38 +0600

    solved

--- P_13.js
+++ P_13.js
@@ -9,3 +9,8 @@
 // method 2
 // let arr = [1, 1, 2, 4, 3, 5];
 // console.log(new Set(arr));
+
+// method 3
+// let arr = [1, 2, 3, 1, 2, 3];
+// let unique_Numbers = [...new Set(arr)];
+// console.log(unique_Numbers);
