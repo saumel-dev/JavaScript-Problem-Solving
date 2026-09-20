569397569efa6708986633b18eec6d5069325902
Saumel-Das <saumel.spr@gmail.com>
2026-09-20 12:35:30 +0600

    solved

--- P_13.js
+++ P_13.js
@@ -1,6 +1,11 @@
-function removeDuplicates(arr) {
-    return arr = arr.filter((num, index) => arr.indexOf(num) === index);
-}
+// method 1
+// function removeDuplicates(arr) {
+//     return arr = arr.filter((num, index) => arr.indexOf(num) === index);
+// }
 
-let new_array = removeDuplicates([1, 2, 2, 3, 3, 4]);
-console.log(new_array);
+// let new_array = removeDuplicates([1, 2, 2, 3, 3, 4]);
+// console.log(new_array);
+
+// method 2
+// let arr = [1, 1, 2, 4, 3, 5];
+// console.log(new Set(arr));
